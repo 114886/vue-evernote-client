0d8885904d8076d13eff112e3fb8a44b2ba639af
114886 <1148864234@qq.com>
2022-02-08 17:43:41 +0800

    笔记本页面基本完成

--- src/components/NotebookList.vue
+++ src/components/NotebookList.vue
@@ -1,20 +1,48 @@
 <template>
-  <div id="notebook-list">
-    <h1>{{ msg }}</h1>
-    <ul>
-      <li><router-link to="/note/1">笔记本1</router-link></li>
-      <li><router-link to="/note/2">笔记本2</router-link></li>
-    </ul>
+  <div class="detail" id="notebook-list">
+    <header>
+      <a href="#" class="btn" @click.prevent="onCreate"
+        ><i class="iconfont icon-plus"></i> 新建笔记本</a
+      >
+    </header>
+    <main>
+      <div class="layout">
+        <h3>笔记本列表({{ notebooks.length }})</h3>
+        <div class="book-list">
+          <router-link
+            v-for="notebook in notebooks"
+            :key="notebook.id"
+            to="/note/1"
+            class="notebook"
+          >
+            <div>
+              <span class="iconfont icon-notebook"></span> {{ notebook.title }}
+              <span>{{ notebook.noteCounts }}</span>
+              <span class="action" @click.stop.prevent="onEdit(notebook)"
+                >编辑</span
+              >
+              <span class="action" @click.stop.prevent="onDelete(notebook)"
+                >删除</span
+              >
+              <span class="date">{{ notebook.friendlyCreatedAt }}</span>
+            </div>
+          </router-link>
+        </div>
+      </div>
+    </main>
   </div>
 </template>
 
 <script>
 import Auth from "@/apis/auth";
+import Notebooks from "@/apis/notebooks";
+import { friendlyDate } from "@/helpers/util";
+
 export default {
   name: "NotebookList",
   data() {
     return {
-      msg: "笔记本列表",
+      notebooks: [],
     };
   },
 
@@ -24,12 +52,76 @@ export default {
         this.$router.push({ path: "/login" });
       }
     });
+
+    Notebooks.getAll().then((res) => {
+      this.notebooks = res.data;
+    });
+  },
+
+  methods: {
+    onCreate() {
+      this.$prompt("输入新笔记本标题", "创建笔记本", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        inputPattern: /^.{1,30}$/,
+        inputErrorMessage: "标题不能为空，且不超过30个字符",
+      })
+        .then(({ value }) => {
+          return Notebooks.addNotebook({ title: value });
+        })
+        .then((res) => {
+          res.data.friendlyCreatedAt = friendlyDate(res.data.createdAt);
+          this.notebooks.unshift(res.data);
+          this.$message.success(res.msg);
+        })
+        .catch((res) => {
+          this.$message.error(res.msg);
+        });
+    },
+
+    onEdit(notebook) {
+      let title = "";
+      this.$prompt("输入新笔记本标题", "修改笔记本", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        inputPattern: /^.{1,30}$/,
+        inputValue: notebook.title,
+        inputErrorMessage: "标题不能为空，且不超过30个字符",
+      })
+        .then(({ value }) => {
+          title = value;
+          return Notebooks.updateNotebook(notebook.id, { title });
+        })
+        .then((res) => {
+          notebook.title = title;
+          this.$message.success(res.msg);
+        })
+        .catch((res) => {
+          this.$message.error(res.msg);
+        });
+    },
+
+    onDelete(notebook) {
+      this.$confirm("确认要删除笔记本吗", "删除笔记本", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          return Notebooks.deleteNotebook(notebook.id);
+        })
+        .then((res) => {
+          this.notebooks.splice(this.notebooks.indexOf(notebook), 1);
+          this.$message.success(res.msg);
+        })
+        .catch((res) => {
+          this.$message.error(res.msg);
+        });
+    },
   },
 };
 </script>
 
-<style scoped>
-h1 {
-  color: red;
-}
+<style lang="less" scoped>
+@import url(../assets/css/notebook-list.less);
 </style>
