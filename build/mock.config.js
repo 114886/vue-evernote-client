45f4b887acbc9926430b70779a4da213b5019bfc
114886 <1148864234@qq.com>
2022-02-11 19:42:00 +0800

    添加自动化部署

--- build/mock.config.js
+++ build/mock.config.js
@@ -1,8 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
-const mockBaseURL = 'https://note-server.hunger-valley.com'
-const realBaseURL = 'https://note-server.hunger-valley.com'
+const mockBaseURL = '//note-server.hunger-valley.com'
+const realBaseURL = '//note-server.hunger-valley.com'
 
 exports.config = function ({ isDev = true } = { isDev: true }) {
   let fileTxt = `
